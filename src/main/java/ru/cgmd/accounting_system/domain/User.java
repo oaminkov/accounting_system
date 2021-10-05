929a902b2060824fff5b7722f7d3926f9f97fb52
oaminkov <oaminkov@gmail.com>
2021-10-05 11:59:04 +0300

    Добавил lombok, произвёл рефакторинг кода

--- src/main/java/ru/cgmd/accounting_system/domain/User.java
+++ src/main/java/ru/cgmd/accounting_system/domain/User.java
@@ -38,10 +38,6 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public boolean isAdmin() {
-        return roles.contains(Role.ADMIN);
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
